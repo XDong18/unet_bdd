02855b43189e1ba6e1db82bae95ebd2254e9a9f9
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-23 13:33:05 +0800

    cc

--- dla_style/unet/unet_model.py
+++ dla_style/unet/unet_model.py
@@ -24,6 +24,7 @@ class UNet(nn.Module):
         self.up3 = Up(256, 128 // factor, bilinear)
         self.up4 = Up(128, 64, bilinear)
         self.outc = OutConv(64, n_classes)
+        self.softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, x):
         x1 = self.inc(x)
@@ -36,4 +37,5 @@ class UNet(nn.Module):
         x = self.up3(x, x2)
         x = self.up4(x, x1)
         logits = self.outc(x)
-        return logits
+        y = self.softmax(logits)
+        return y
