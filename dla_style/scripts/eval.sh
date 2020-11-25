b9361443654a64c36085025662e631cc286506c5
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-25 10:53:28 +0800

    cc

--- dla_style/scripts/eval.sh
+++ dla_style/scripts/eval.sh
@@ -2,4 +2,4 @@ export CUDA_VISIBLE_DEVICES=4,5,6,7
 python3 segment.py train -e -d /shared/xudongliu/bdd100k/seg/seg -c 19 -s 768 --arch dla102up \
     --batch-size 8 --lr 0.01 --momentum 0.9 --lr-mode poly \
     --epochs 500 --random-scale 2 --random-rotate 0 \
-    --random-color --pretrained-base imagenet -o out/unet_500e resume out/unet_500e/model_best.pth.tar
+    --random-color --pretrained-base imagenet -o out/unet_500e --resume out/unet_500e/model_best.pth.tar
